e3d125d6e4642dbda65033874639e175267f1a1a
MilanMareels <milan.mareels@gmail.com>
2026-02-01 20:19:18 +0100

    feat: last change

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="min-h-screen pt-20 flex items-center justify-center relative overflow-hidden">
-      <div className="absolute inset-0 z-0">
-        <img src="https://theeyeopener.com/wp-content/uploads/2022/09/Redbullgaminghub_vkauk_sept22.jpeg" alt="Gaming Background" className="absolute inset-0 w-full h-full object-cover opacity-40" />
-        <div className="absolute inset-0 bg-linear-to-b from-slate-950/20 via-slate-950/60 to-slate-950"></div>
-      </div>
+      <div className="absolute inset-0 z-0 bg-linear-to-b from-gray-200 to-slate-950" />
 
       <div className="container mx-auto px-4 z-10 text-center">
         <h1 className="text-6xl md:text-8xl font-black tracking-tighter mb-6">
